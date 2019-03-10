9df39464d3ac5a6ae4c33bd7f9cac2cfbeebe7be
jonas32 <j.breuer@fz-juelich.de>
2019-03-10 17:11:29 +0100

    Demomomap Fix

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,5 +1,5 @@
 import Navbar from "./Navbar";
-import React, { useRef, useContext, useEffect } from "react";
+import React, { useState, useRef, useContext, useEffect } from "react";
 import leaflet from "leaflet";
 import geojs from "../mapcoords.json";
 import moment from "moment";
@@ -7,77 +7,81 @@ import { LanguageContext } from "../contexts/LanguageContext";
 
 function Demos() {
   const ctx = useContext(LanguageContext);
+  const [count, setCount] = useState(0);
   const eventlist = useRef(null);
   useEffect(() => {
-    const demomap = leaflet.map("demomap");
-    leaflet
-      .tileLayer(
-        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
-        {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL | Save the Internet!'
+    if (count === 0) {
+      const demomap = leaflet.map("demomap");
+      leaflet
+        .tileLayer(
+          "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+          {
+            attribution:
+              '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL | Save the Internet!'
+          }
+        )
+        .addTo(demomap);
+
+      const STIIcon = leaflet.icon({
+        iconUrl: "/pin_sti.png",
+        iconSize: [32, 32],
+        iconAnchor: [16, 37],
+        popupAnchor: [0, -28]
+      });
+      const nonSTIIcon = leaflet.icon({
+        iconUrl: "/pin_blank.png",
+        iconSize: [32, 32],
+        iconAnchor: [16, 37],
+        popupAnchor: [0, -28]
+      });
+
+      function bindPopoup(feature, layer) {
+        let popupText = "";
+
+        for (let index = 0; index < feature.properties.length; index++) {
+          const element = feature.properties[index];
+          if (element.fa_icon === "fa-clock-o") {
+            let momentObj = moment(element.value, "DD.MM.YYYY H:m");
+
+            // Get locale data
+            var localeData = moment.localeData(ctx.language);
+            var format = localeData.longDateFormat("LLL");
+            // Remove year part
+            format = format.replace(/.YYYY/, "");
+            element.value = momentObj.locale(ctx.language).format(format);
+          }
+          popupText += `<p class="mb-0 font-thin"><i class="fa ${
+            element.fa_icon
+          }" aria-hidden="true"></i> ${element.value}</p>`;
         }
-      )
-      .addTo(demomap);
 
-    const STIIcon = leaflet.icon({
-      iconUrl: "/pin_sti.png",
-      iconSize: [32, 32],
-      iconAnchor: [16, 37],
-      popupAnchor: [0, -28]
-    });
-    const nonSTIIcon = leaflet.icon({
-      iconUrl: "/pin_blank.png",
-      iconSize: [32, 32],
-      iconAnchor: [16, 37],
-      popupAnchor: [0, -28]
-    });
-
-    function bindPopoup(feature, layer) {
-      let popupText = "";
-
-      for (let index = 0; index < feature.properties.length; index++) {
-        const element = feature.properties[index];
-        if (element.fa_icon === "fa-clock-o") {
-          let momentObj = moment(element.value, "DD.MM.YYYY H:m");
-
-          // Get locale data
-          var localeData = moment.localeData(ctx.language);
-          var format = localeData.longDateFormat("LLL");
-          // Remove year part
-          format = format.replace(/.YYYY/, "");
-          element.value = momentObj.locale(ctx.language).format(format);
-        }
-        popupText += `<p class="mb-0 font-thin"><i class="fa ${
-          element.fa_icon
-        }" aria-hidden="true"></i> ${element.value}</p>`;
+        let listText = `<li class="shadow-md p-2 mb-4 rounded-lg cursor-pointer" id="markerListItem" data-latlang="${
+          feature.geometry.coordinates
+        }">${popupText}</li>`;
+        let popup = layer.bindPopup(popupText);
+        popup.on("click", function(e) {
+          demomap.flyTo(e.latlng, 14, {
+            animate: true,
+            duration: 1.5
+          });
+        });
+        //jQuery("#event-list").append(listText);
       }
 
-      let listText = `<li class="shadow-md p-2 mb-4 rounded-lg cursor-pointer" id="markerListItem" data-latlang="${
-        feature.geometry.coordinates
-      }">${popupText}</li>`;
-      let popup = layer.bindPopup(popupText);
-      popup.on("click", function(e) {
-        demomap.flyTo(e.latlng, 14, {
-          animate: true,
-          duration: 1.5
-        });
-      });
-      //jQuery("#event-list").append(listText);
+      let geoJSONLayer = leaflet
+        .geoJSON(geojs, {
+          pointToLayer: function(feature, latlng) {
+            if (feature["STIDemo"]) {
+              return leaflet.marker(latlng, { icon: STIIcon });
+            }
+            return leaflet.marker(latlng, { icon: nonSTIIcon });
+          },
+          onEachFeature: bindPopoup
+        })
+        .addTo(demomap);
+      demomap.fitBounds(geoJSONLayer.getBounds());
+      setCount(1);
     }
-
-    let geoJSONLayer = leaflet
-      .geoJSON(geojs, {
-        pointToLayer: function(feature, latlng) {
-          if (feature["STIDemo"]) {
-            return leaflet.marker(latlng, { icon: STIIcon });
-          }
-          return leaflet.marker(latlng, { icon: nonSTIIcon });
-        },
-        onEachFeature: bindPopoup
-      })
-      .addTo(demomap);
-    demomap.fitBounds(geoJSONLayer.getBounds());
   });
 
   return (
