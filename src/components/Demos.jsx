db9ddc7d4ba1f18b32609829503239794f1bb15f
jonas32 <j.breuer@fz-juelich.de>
2019-03-10 17:09:12 +0100

    Demos

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,9 +1,13 @@
 import Navbar from "./Navbar";
-import React, { useEffect } from "react";
+import React, { useRef, useContext, useEffect } from "react";
 import leaflet from "leaflet";
 import geojs from "../mapcoords.json";
+import moment from "moment";
+import { LanguageContext } from "../contexts/LanguageContext";
 
 function Demos() {
+  const ctx = useContext(LanguageContext);
+  const eventlist = useRef(null);
   useEffect(() => {
     const demomap = leaflet.map("demomap");
     leaflet
@@ -29,6 +33,39 @@ function Demos() {
       popupAnchor: [0, -28]
     });
 
+    function bindPopoup(feature, layer) {
+      let popupText = "";
+
+      for (let index = 0; index < feature.properties.length; index++) {
+        const element = feature.properties[index];
+        if (element.fa_icon === "fa-clock-o") {
+          let momentObj = moment(element.value, "DD.MM.YYYY H:m");
+
+          // Get locale data
+          var localeData = moment.localeData(ctx.language);
+          var format = localeData.longDateFormat("LLL");
+          // Remove year part
+          format = format.replace(/.YYYY/, "");
+          element.value = momentObj.locale(ctx.language).format(format);
+        }
+        popupText += `<p class="mb-0 font-thin"><i class="fa ${
+          element.fa_icon
+        }" aria-hidden="true"></i> ${element.value}</p>`;
+      }
+
+      let listText = `<li class="shadow-md p-2 mb-4 rounded-lg cursor-pointer" id="markerListItem" data-latlang="${
+        feature.geometry.coordinates
+      }">${popupText}</li>`;
+      let popup = layer.bindPopup(popupText);
+      popup.on("click", function(e) {
+        demomap.flyTo(e.latlng, 14, {
+          animate: true,
+          duration: 1.5
+        });
+      });
+      //jQuery("#event-list").append(listText);
+    }
+
     let geoJSONLayer = leaflet
       .geoJSON(geojs, {
         pointToLayer: function(feature, latlng) {
@@ -36,12 +73,13 @@ function Demos() {
             return leaflet.marker(latlng, { icon: STIIcon });
           }
           return leaflet.marker(latlng, { icon: nonSTIIcon });
-        }
-        //onEachFeature: bindPopoup
+        },
+        onEachFeature: bindPopoup
       })
       .addTo(demomap);
     demomap.fitBounds(geoJSONLayer.getBounds());
   });
+
   return (
     <div>
       <React.Fragment>
@@ -53,6 +91,7 @@ function Demos() {
           style={{ height: window.innerHeight / 1.8 + "px" }}
           id="demomap"
         />
+        <div className="" ref={eventlist} />
       </React.Fragment>
     </div>
   );
