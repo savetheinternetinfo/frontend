9aee6097019bbf6259cb0199cd94dc71d2c30c7c
lynx <me@lynx.pub>
2019-03-15 21:35:19 +0100

    fix margin/padding imprint/policy

--- src/components/Imprint.jsx
+++ src/components/Imprint.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import { useStateValue } from '../contexts/StateContext'
+import React from "react";
+import { useStateValue } from "../contexts/StateContext";
 
 function Imprint() {
-  const [{translation}] = useStateValue();
+  const [{ translation }] = useStateValue();
 
   return (
-    <div className="container mx-auto py-16">
+    <div className="container mx-auto -mt-10 px-6 py-8">
       <h1>Impressum</h1>
       <p>
-        Savetheinternet.info<br />
-        Dominic Kis<br />
-        Sofienstraße 22<br />
-        76461 Muggensturm<br />
+        Savetheinternet.info
+        <br />
+        Dominic Kis
+        <br />
+        Sofienstraße 22
+        <br />
+        76461 Muggensturm
+        <br />
         Germany
       </p>
       <p>E-Mail: info@savetheinternet.info</p>
       <p>
-      Platform of the European Commission for online dispute resolution: <a href="http://ec.europa.eu/consumers/odr">http://ec.europa.eu/consumers/odr</a>
-      <br />
-      We are neither obligated nor willing to take part in a Dispute Resolution before Independent Consumer Arbitration Service.
+        Platform of the European Commission for online dispute resolution:{" "}
+        <a href="http://ec.europa.eu/consumers/odr">
+          http://ec.europa.eu/consumers/odr
+        </a>
+        <br />
+        We are neither obligated nor willing to take part in a Dispute
+        Resolution before Independent Consumer Arbitration Service.
       </p>
     </div>
   );
