a54c18775aaf98ed321e9de006a748d8c269e41f
lynx <me@lynx.pub>
2019-03-15 12:43:11 +0100

    exchanging dangerouslysetinnerhtml with html-react-parser

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,33 +1,36 @@
 import React from "react";
-import {useStateValue} from '../contexts/StateContext';
+import parse from "html-react-parser";
+
+import { useStateValue } from "../contexts/StateContext";
 
 function About() {
-  const [{translation}] = useStateValue();
+  const [{ translation }] = useStateValue();
 
   return (
     <div className="container mx-auto py-16">
       <h1>{translation.about_us_heading}</h1>
-      
+
       <h3>{translation.about_us_1_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_1_text}}></p>
-      
+
+      <p>{parse(translation.about_us_1_text)}</p>
+
       <h3>{translation.about_us_2_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_2_text}}></p>
-      
+      <p>{parse(translation.about_us_2_text)}</p>
+
       <h3>{translation.about_us_3_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_3_text}}></p>
-      
+      <p>{parse(translation.about_us_3_text)}</p>
+
       <h3>{translation.about_us_4_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_4_text}}></p>
-      
+      <p>{parse(translation.about_us_4_text)}</p>
+
       <h3>{translation.about_us_5_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_5_text}}></p>
-      
+      <p>{parse(translation.about_us_5_text)}</p>
+
       <h3>{translation.about_us_6_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_6_text}}></p>
-      
+      <p>{parse(translation.about_us_6_text)}</p>
+
       <h3>{translation.about_us_7_heading}</h3>
-      <p dangerouslySetInnerHTML={{__html: translation.about_us_7_text}}></p>
+      <p>{parse(translation.about_us_7_text)}</p>
     </div>
   );
 }
