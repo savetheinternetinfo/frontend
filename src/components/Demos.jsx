bccd22e705ae21adf78c4aca0c5d5f9a2760a66f
Jakob Schrettenbrunner <dev@schrej.net>
2019-03-18 00:30:21 +0100

    add additional section to Home fix Demos useEffect

--- src/components/Demos.jsx
+++ src/components/Demos.jsx
@@ -1,183 +1,176 @@
-import React, {useState, useEffect} from "react";
-import {useStateValue} from "../contexts/StateContext";
-import config from '../config';
+import React, { useState, useEffect } from "react";
+import { useStateValue } from "../contexts/StateContext";
+import config from "../config";
 
 import parse from "html-react-parser";
 import leaflet from "leaflet";
 //import geojs from "../mapcoords.json";
 import moment from "moment";
-import axios from 'axios';
+import axios from "axios";
 
 function uniqueId(str) {
-    return `${str}${Math.random()
-        .toString(36)
-        .substr(2, 9)}`;
+  return `${str}${Math.random()
+    .toString(36)
+    .substr(2, 9)}`;
 }
 
 function Demos() {
-    const [{language}] = useStateValue();
+  const [{ language }] = useStateValue();
 
-    const [count, setCount] = useState(0);
-    const [loading, setLoading] = useState(true);
-    const [events, setEvents] = useState([]);
-    useEffect(() => {
-        if (count === 0) {
-            const demomap = leaflet.map("demomap", {
-                scrollWheelZoom: false
-            });
-            leaflet
-                .tileLayer(
-                    "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
-                    {
-                        attribution:
-                            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL | Save the Internet!'
-                    }
-                )
-                .addTo(demomap);
-
-            const STIIcon = leaflet.icon({
-                iconUrl: "/pin_sti.png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 37],
-                popupAnchor: [0, -28]
-            });
-            const nonSTIIcon = leaflet.icon({
-                iconUrl: "/pin_blank.png",
-                iconSize: [32, 32],
-                iconAnchor: [16, 37],
-                popupAnchor: [0, -28]
-            });
-
-            const geojs = {
-                "type": "FeatureCollection",
-                "features": [],
-            };
-
-
-            function bindPopoup(feature, layer) {
-                let popupText = "";
-                let eventItem = [];
-                for (let index = 0; index < feature.properties.length; index++) {
-                    const element = feature.properties[index];
-                    if (element.fa_icon === "fa-clock") {
-                        let momentObj = moment(element.value);
-
-                        // Get locale data
-                        var localeData = moment.localeData(language);
-                        var format = localeData.longDateFormat("LLL");
-                        // Remove year part
-                        format = format.replace(/.YYYY/, "");
-                        element.value = momentObj.locale(language).format(format);
-                    }
-                    eventItem.push({icon: element.fa_icon, value: element.value});
-                    popupText += `<p className="mb-0 font-thin">
-                                    <i class="w-4 fa ${element.fa_icon}" aria-hidden="true"></i> ${element.value}
-                                  </p>`;
-                }
-                let tmp = events;
-                tmp.push(eventItem);
-                setEvents(tmp);
-
-                let popup = layer.bindPopup(popupText);
-                popup.on("click", function (e) {
-                    demomap.flyTo(e.latlng, 14, {
-                        animate: true,
-                        duration: 1.5
-                    });
-                });
-            }
-
-            axios.get(config.api.points)
-                .then(response => {
-                    geojs.features = response.data.points.map(item => {
-                        return {
-                            type: "Feature",
-                            properties: [
-                                {
-                                    fa_icon: "fa-map-marker",
-                                    value: item.location,
-                                },
-                                {
-                                    fa_icon: "fa-clock",
-                                    value: item.time,
-                                },
-                                {
-                                    fa_icon: "fb_event",
-                                    value: item.facebookEvent || '',
-                                }
-                            ],
-                            STIDemo: item.sti_event,
-                            geometry: {
-                                type: "Point",
-                                coordinates: [
-                                    item.latitude,
-                                    item.longitude,
-                                ]
-                            }
-                        };
-                    });
-
-                    let geoJSONLayer = leaflet
-                        .geoJSON(geojs, {
-                            pointToLayer: function (feature, latlng) {
-                                if (feature["STIDemo"]) {
-                                    return leaflet.marker(latlng, {icon: STIIcon});
-                                }
-                                return leaflet.marker(latlng, {icon: nonSTIIcon});
-                            },
-                            onEachFeature: bindPopoup
-                        })
-                        .addTo(demomap);
-                    demomap.fitBounds(geoJSONLayer.getBounds());
-                    setLoading(false);
-                })
-                .catch(error => {
-                    setLoading(false);
-                });
-
-
-            setCount(1);
+  const [loading, setLoading] = useState(true);
+  const [events, setEvents] = useState([]);
+  useEffect(() => {
+    const demomap = leaflet.map("demomap", {
+      scrollWheelZoom: false
+    });
+    leaflet
+      .tileLayer(
+        "https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png",
+        {
+          attribution:
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Map tiles by Carto, under CC BY 3.0. Data by OpenStreetMap, under ODbL | Save the Internet!'
         }
+      )
+      .addTo(demomap);
+
+    const STIIcon = leaflet.icon({
+      iconUrl: "/pin_sti.png",
+      iconSize: [32, 32],
+      iconAnchor: [16, 37],
+      popupAnchor: [0, -28]
+    });
+    const nonSTIIcon = leaflet.icon({
+      iconUrl: "/pin_blank.png",
+      iconSize: [32, 32],
+      iconAnchor: [16, 37],
+      popupAnchor: [0, -28]
     });
 
-    return (
-        <div>
-            <React.Fragment>
-                <div className="flex flex-no-wrap p-2 w-full overflow-x-scroll bg-blue">
-                    {events.map(x => {
-                        return (
-                            <div
-                                key={uniqueId("key-")}
-                                className="flex-none p-2"
-                                style={{width: "15rem"}}
-                            >
-                                <div className="bg-blue-dark text-white rounded p-4 w-full h-full shadow rounded">
-                                    {x.map(e => {
-                                        return (
-                                            <p key={uniqueId("key-")} className="font-thin mt-2">
-                                                <span className="flex">
-                                                  <i
-                                                      className={"w-8 fa " + e.icon}
-                                                      aria-hidden="true"
-                                                  />
-                                                    {parse(e.value)}
-                                                </span>
-                                            </p>
-                                        );
-                                    })}
-                                </div>
-                            </div>
-                        );
-                    })}
+    const geojs = {
+      type: "FeatureCollection",
+      features: []
+    };
+
+    function bindPopoup(feature, layer) {
+      let popupText = "";
+      let eventItem = [];
+      for (let index = 0; index < feature.properties.length; index++) {
+        const element = feature.properties[index];
+        if (element.fa_icon === "fa-clock") {
+          let momentObj = moment(element.value);
+
+          // Get locale data
+          var localeData = moment.localeData(language);
+          var format = localeData.longDateFormat("LLL");
+          // Remove year part
+          format = format.replace(/.YYYY/, "");
+          element.value = momentObj.locale(language).format(format);
+        }
+        eventItem.push({ icon: element.fa_icon, value: element.value });
+        popupText += `<p className="mb-0 font-thin">
+                                    <i class="w-4 fa ${
+                                      element.fa_icon
+                                    }" aria-hidden="true"></i> ${element.value}
+                                  </p>`;
+      }
+      let tmp = events;
+      tmp.push(eventItem);
+      setEvents(tmp);
+
+      let popup = layer.bindPopup(popupText);
+      popup.on("click", function(e) {
+        demomap.flyTo(e.latlng, 14, {
+          animate: true,
+          duration: 1.5
+        });
+      });
+    }
+
+    axios
+      .get(config.api.points)
+      .then(response => {
+        geojs.features = response.data.points.map(item => {
+          return {
+            type: "Feature",
+            properties: [
+              {
+                fa_icon: "fa-map-marker",
+                value: item.location
+              },
+              {
+                fa_icon: "fa-clock",
+                value: item.time
+              },
+              {
+                fa_icon: "fb_event",
+                value: item.facebookEvent || ""
+              }
+            ],
+            STIDemo: item.sti_event,
+            geometry: {
+              type: "Point",
+              coordinates: [item.latitude, item.longitude]
+            }
+          };
+        });
+
+        let geoJSONLayer = leaflet
+          .geoJSON(geojs, {
+            pointToLayer: function(feature, latlng) {
+              if (feature["STIDemo"]) {
+                return leaflet.marker(latlng, { icon: STIIcon });
+              }
+              return leaflet.marker(latlng, { icon: nonSTIIcon });
+            },
+            onEachFeature: bindPopoup
+          })
+          .addTo(demomap);
+        demomap.fitBounds(geoJSONLayer.getBounds());
+        setLoading(false);
+      })
+      .catch(error => {
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <React.Fragment>
+        <div className="flex flex-no-wrap p-2 w-full overflow-x-scroll bg-blue">
+          {events.map(x => {
+            return (
+              <div
+                key={uniqueId("key-")}
+                className="flex-none p-2"
+                style={{ width: "15rem" }}
+              >
+                <div className="bg-blue-dark text-white rounded p-4 w-full h-full shadow rounded">
+                  {x.map(e => {
+                    return (
+                      <p key={uniqueId("key-")} className="font-thin mt-2">
+                        <span className="flex">
+                          <i
+                            className={"w-8 fa " + e.icon}
+                            aria-hidden="true"
+                          />
+                          {parse(e.value)}
+                        </span>
+                      </p>
+                    );
+                  })}
                 </div>
-                <div
-                    className="w-full bg-grey-lightest z-10"
-                    style={{height: window.innerHeight / 1.3 + "px"}}
-                    id="demomap"
-                />
-            </React.Fragment>
+              </div>
+            );
+          })}
         </div>
-    );
+        <div
+          className="w-full bg-grey-lightest z-10"
+          style={{ height: window.innerHeight / 1.3 + "px" }}
+          id="demomap"
+        />
+      </React.Fragment>
+    </div>
+  );
 }
 
 export default Demos;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -31,7 +31,7 @@ function Home() {
           </p>
         </div>
       </div>
-      <div className="bg-orange text-blue-dark text-center">
+      <div className="bg-white text-blue-dark text-center">
         <div className="container mx-auto flex my-16">
           <div className="h-full pr-12 border-r border-blue-dark">
             <h2>Article 11</h2>
@@ -90,7 +90,6 @@ function Home() {
           <div className="flex text-center mt-16">
             <div className="h-full pr-12 border-r border-blue">
               <h2>Sign the petition</h2>
-              <p>summary of article 11</p>
               <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
@@ -107,7 +106,6 @@ function Home() {
             </div>
             <div className="h-full px-12 border-r border-blue">
               <h2>Go to a demonstration</h2>
-              <p>summary of article 11</p>
               <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
@@ -124,7 +122,6 @@ function Home() {
             </div>
             <div className="h-full pl-12 ">
               <h2>Spread the word</h2>
-              <p>summary of article 13</p>
               <p>
                 Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
                 diam nonumy eirmod tempor invidunt ut labore et dolore magna
@@ -170,6 +167,38 @@ function Home() {
                 />
               </div>
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white text-blue-dark">
+        <div className="container mx-auto my-16">
+          <div className="my-16">
+            <h1>What happened so far?</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
+            <h1>Article </h1>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet.
+            </p>
           </div>
         </div>
       </div>
