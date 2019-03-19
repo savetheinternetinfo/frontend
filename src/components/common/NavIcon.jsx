447aeca4692eef91b88f474894fd8290268695bb
lynx <me@lynx.pub>
2019-03-19 18:31:10 +0100

    slight navicon/url placement correction

--- src/components/common/NavIcon.jsx
+++ src/components/common/NavIcon.jsx
@@ -5,15 +5,18 @@ import * as Feather from "react-feather";
 function NavIcon({ icon, route, url, size, className, onClick }) {
   const IconComponent = Feather[icon];
   const iconSize = size || 16;
+  const styles = {
+    margin: "0.4rem"
+  };
   const classes =
-    "bezier focus:outline-none text-white hover:text-orange cursor-pointer text-center px-2 py-2 m-2 select-none " +
+    "bezier focus:outline-none text-white hover:text-orange cursor-pointer text-center px-2 py-2 select-none " +
     className;
   return typeof route === "undefined" ? (
-    <a href={url} className={classes} onClick={onClick}>
+    <a href={url} style={styles} className={classes} onClick={onClick}>
       <IconComponent size={iconSize} />
     </a>
   ) : (
-    <Link to={route} className={classes} onClick={onClick}>
+    <Link to={route} style={styles} className={classes} onClick={onClick}>
       <IconComponent size={iconSize} />
     </Link>
   );
--- src/components/common/NavURL.jsx
+++ src/components/common/NavURL.jsx
@@ -3,10 +3,17 @@ import { NavLink } from "react-router-dom";
 
 function NavURL({ text, route, onClick, className }) {
   const classes =
-    "underline text-white text-center uppercase px-2 py-2 m-2 no-underline select-none " +
+    "underline text-white text-center uppercase px-2 py-1 m-2 no-underline select-none " +
     className;
   return (
-    <NavLink to={route} className={classes} onClick={onClick}>
+    <NavLink
+      style={{
+        paddingTop: "0.3rem"
+      }}
+      to={route}
+      className={classes}
+      onClick={onClick}
+    >
       {text}
     </NavLink>
   );
